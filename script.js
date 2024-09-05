53f57fd1a9cc90561e706e979071d67d5c425453
HasanAmmar05 <altalib.hasan05@gmail.com>
2024-09-05 17:19:55 +0800

    Added function for adding circles/squares on click

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const gameBoard = document.querySelector("#gameboard");
 const InfoDisplay = document.querySelector("#info");
+let go = "circle";
 const startCells = [
     "", "", "", 
     "", "", "", 
@@ -9,11 +10,21 @@ const startCells = [
 InfoDisplay.textContent = "Circle goes first";
 
 function createBoard() {
-    startCells.forEach((cell, index) => {
+    startCells.forEach((_cell, index) => {
         const cellElement = document.createElement("div");
         cellElement.classList.add("square");
+        cellElement.id = index;
+
+        cellElement.addEventListener("click", addGo);
+
         gameBoard.append(cellElement);
-    }) 
+    });
 }
 
 createBoard();
+
+function addGo(e) {
+    const goDisplay = document.createElement("div");
+    goDisplay.classList.add(go);
+    e.target.append(goDisplay);
+}
